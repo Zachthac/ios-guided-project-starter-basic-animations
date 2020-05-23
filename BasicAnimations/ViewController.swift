1cbca9fbc02449b43c2ab148b1b27858d3eb961e
Your Name Here <youremailhere>
2020-05-23 16:23:54 -0400

    set up buttons and labels

--- BasicAnimations/ViewController.swift
+++ BasicAnimations/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .brown
+        configureLabel()
+        configureButtons()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -25,15 +27,47 @@ class ViewController: UIViewController {
     }
 
     private func configureLabel() {
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        label.layer.borderWidth = 8
+        label.backgroundColor = .white
+        label.layer.cornerRadius = 12
+        label.text = "💩"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 96)
         
+        view.addSubview(label)
+        
+        label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
     }
     
+    
     private func configureButtons() {
+        let rotateButton = UIButton(type: .system)
+        rotateButton.translatesAutoresizingMaskIntoConstraints = false
+        rotateButton.setTitle("Rotate", for: .normal)
+        rotateButton.addTarget(self, action: #selector(rotateButtonTapped), for: .touchUpInside)
+        rotateButton.tintColor = .black
         
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(rotateButton)
+        
+        NSLayoutConstraint.activate(
+            [
+                stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+                stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+                stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+            ]
+        )
     }
     
     @objc private func rotateButtonTapped() {
-        
+        label.center = view.center
+        UIView.animate(withDuration: 2.0, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
     }
     
     @objc private func springButtonTapped() {
